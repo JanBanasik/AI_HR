d18dea084bba615c281d90008be2422c3e530940
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-04-12 14:40:52 +0200

    Creating prompt

--- codeAnalysis/codingStandardsAnalysis.py
+++ codeAnalysis/codingStandardsAnalysis.py
@@ -1,11 +1,12 @@
 import requests
 from impl import scrape_github_user_info
 
+
 def createPrompt(commit) -> str:
     return f"""
     **Act as a Senior Software Engineer performing a code review.**  
     Analyze the following code changes from GitHub commit in repository '{commit['repo']}' (Date: {commit['date']}):
-    
+    Don't provide any updated versions of the code, just analyze it 
     **Commit Message:**  
     {commit['message']}
     
@@ -21,9 +22,9 @@ headers = {
     "Content-Type": "application/json"
 }
 
-
-
+evalsByLanguage = {}
 for key, value in commit["commits_by_language"].items():
+    scores = []
     for commit in value:
         prompt = createPrompt(commit)
         data = {
@@ -33,4 +34,25 @@ for key, value in commit["commits_by_language"].items():
         }
 
         response = requests.post(url, headers=headers, json=data)
-        print(response.json()['choices'][0]['message']['content'])
+        try:
+            scores.append(response.json()["choices"][0]["choice"])
+        except KeyError as e:
+            continue
+
+    res = '\n'.join(scores)
+    overallPrompt = f"Based on those scores, please evaulate the candidate in the realm of {key} programming language: {res}"
+    data = {
+        "model": "llama3-8b-8192",
+        "messages": [{"role": "user", "content": f"{overallPrompt}"}],
+        "temperature": 0.7
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        evalsByLanguage[key] = response.json()["choices"][0]["choice"]
+    except Exception as e:
+        print(f"Error for: {key} language")
+        print(e)
+
+for language, evals in evalsByLanguage.items():
+    print(language, evals)
