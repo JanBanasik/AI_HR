3865e1321ef66b00bf70e517dcfd115450d36c05
fbaci <fbaciak@gmail.com>
2025-04-12 14:39:48 +0200

    Prompt changing

--- codeAnalysis/codingStandardsAnalysis.py
+++ codeAnalysis/codingStandardsAnalysis.py
@@ -1,16 +1,7 @@
 import requests
 from impl import scrape_github_user_info
+from create_prompt import createPrompt
 
-def createPrompt(commit) -> str:
-    return f"""
-    **Act as a Senior Software Engineer performing a code review.**  
-    Analyze the following code changes from GitHub commit in repository '{commit['repo']}' (Date: {commit['date']}):
-    
-    **Commit Message:**  
-    {commit['message']}
-    
-    **Code Changes:**
-    {commit['code_diff']}"""
 
 
 commit = scrape_github_user_info("JanBanasik")
