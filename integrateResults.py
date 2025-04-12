8d3ff58de1aafe240a228dca4c850781c3b245f2
JanBanasik <jan.jerzy.banasik@gmail.com>
2025-04-12 22:15:05 +0200

    integration complete success

--- integrateResults.py
+++ integrateResults.py
@@ -1,6 +1,17 @@
-from codeAnalysis.getCodeReview import  getResultsForGivenUserName
+from codeAnalysis.getCodeReview import getResultsForGivenUserName
 from X.main import getPersonSentimentEvaluation
+import os
 
 
-s = getPersonSentimentEvaluation("person001")
-getResultsForGivenUserName("Kubapatimat", "data/person001-code.json")
+def mergeResults(githubUsername="Kubapatimat", twitterId="person001"):
+    if not os.path.exists("data/"):
+        os.makedirs("data/")
+
+    outputJson = f"data/evaluation.json"
+
+    twitterSentimentAnalysis = getPersonSentimentEvaluation(twitterId)
+    getResultsForGivenUserName(githubUsername, outputJson)
+
+
+if __name__ == "__main__":
+    mergeResults(githubUsername="Kubapatimat", twitterId="person001")
